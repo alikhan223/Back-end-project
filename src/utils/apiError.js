38c68ecb6add2369e6f87e3f5d04259dc3856f3e
Ali Shah <alibro2023khan@gmail.com>
2024-12-20 18:17:49 +0500

    utils wraper code

---
+++ src/utils/apiError.js
@@ -0,0 +1,25 @@
+class apiError extends Error {
+    constructor(
+        statusCode,
+        message = "Some thing went wrong",
+        errors = [],
+        statck = ""
+    ){
+        super(message)
+        this.statusCode = statusCode,
+        this.message = message,
+        this.errors = errors,
+        this.data = null,
+        this.success = false
+
+
+        if (statck) {
+            this.statck = statck
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+
+    }
+}
+
+export {apiError}
